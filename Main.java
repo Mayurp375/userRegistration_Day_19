4d7bdb491a938c9dbd72e8d8fabed53ee7c21340
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-10 20:30:55 +0530

    main File

---
+++ Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("User Registration");
+       /* firstName obj = new firstName();
+        System.out.println(obj);*/
+
+        /*lastName obj1 = new lastName();
+        System.out.println(obj1);*/
+
+      /*  email_ID obj3 = new email_ID();
+        System.out.println(obj3);*/
+
+      /*  mobileNumber obj4 = new mobileNumber();
+        System.out.println(obj4);*/
+
+        PasswordRull obj5 = new PasswordRull();
+        System.out.println(obj5);
+    }
+}
